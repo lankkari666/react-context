5a5a1362471b6a66dba7825e609299b4d6085162
cultkkari <osv1313006@gmail.com>
2024-02-24 21:30:12 +0300

    second commit

---
+++ List/list.jsx
@@ -0,0 +1,27 @@
+import { SectionList } from '../SectionList/SectionList.jsx';
+import { useApp } from '../../context/context.jsx';
+
+
+export const List = () => {
+
+	const { setTasks, tasks } = useApp();
+
+	return (
+		<div className="list">
+			<ul>
+				{tasks.map((item) => {
+					return (
+						<SectionList
+							key={item.id}
+							check={item.check}
+							index={item.id}
+							funcSet={setTasks}
+						>
+							{item.text}
+						</SectionList>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+export * from './SectionAdd/SectionAdd.jsx';
+export * from './SectionList/SectionList.jsx';
+export * from './List/list.jsx';
+export * from './axiosReq.js';
